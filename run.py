f896174bfc2aadb09c2508600419dfd29d69d06d
mlangkabel <malte.langkabel@gmail.com>
2019-01-21 12:51:52 +0100

    add "--verbose" flag that configures printing of log messages

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ def main():
 	parser.add_argument('--database-file-path', help='path to the generated Sourcetrail database file', type=str, required=True)
 	parser.add_argument('--source-file-path', help='path to the source file to index', type=str, required=True)
 	parser.add_argument('--clear', help='clear the database before indexing', action='store_true', required=False)
+	parser.add_argument('--verbose', help='enable verbose console output', action='store_true', required=False)
 
 	args = parser.parse_args()
 	databaseFilePath = args.database_file_path
@@ -19,29 +20,32 @@ def main():
 		print('ERROR: ' + srctrl.getLastError())
 	
 	if args.clear:
-		print('Clearing database...')
+		if args.verbose:
+			print('Clearing database...')
 		if not srctrl.clear():
 			print('ERROR: ' + srctrl.getLastError())
 		else:
-			print('Clearing done.')
+			if args.verbose:
+				print('Clearing done.')
 
-	if srctrl.isEmpty():
-		print('Loaded database is empty.')
-	else:
-		print('Loaded database contains data.')
+	if args.verbose:
+		if srctrl.isEmpty():
+			print('Loaded database is empty.')
+		else:
+			print('Loaded database contains data.')
 
 	srctrl.beginTransaction()
-	indexSourceFile(sourceFilePath)
+	indexSourceFile(sourceFilePath, args.verbose)
 	srctrl.commitTransaction()
 
 	if not srctrl.close():
 		print('ERROR: ' + srctrl.getLastError())
 
 
-def indexSourceFile(sourceFilePath):
+def indexSourceFile(sourceFilePath, verbose):
 	workingDirectory = os.getcwd()
 	astVisitorClient = indexer.AstVisitorClient()
-	indexer.indexSourceFile(sourceFilePath, workingDirectory, astVisitorClient, True)
+	indexer.indexSourceFile(sourceFilePath, workingDirectory, astVisitorClient, verbose)
 
 
 if __name__ == '__main__':
